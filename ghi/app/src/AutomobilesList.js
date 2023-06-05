4a00ad8b7829a200f28ed9c1caee88b8b1ca151f
Henry Kim <hnrykm@gmail.com>
2023-06-05 18:53:33 -0400

    Automobile List completed

--- ghi/app/src/AutomobilesList.js
+++ ghi/app/src/AutomobilesList.js
@@ -1,7 +1,86 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './index.css';
 
 function AutomobilesList() {
-    return 
+	const [automobileList, setAutomobileList] = useState([]);
+
+	const fetchData = async () => {
+		const url = 'http://localhost:8100/api/automobiles/';
+		const response = await fetch(url);
+
+		if (response.ok) {
+			const data = await response.json();
+			setAutomobileList(data);
+		}
+	};
+
+	const isAvailable = (sold) => {
+		if (sold == true) {
+			return (
+				<div class="alert alert-danger m-0 p-0" role="alert">
+					Sold
+				</div>
+			);
+		} else {
+			return (
+				<div class="alert alert-success m-0 p-0" role="alert">
+					Available
+				</div>
+			);
+		}
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return (
+		<div className="row">
+			<div className="offset-1 col-10">
+				<div className="shadow p-3 m-4">
+					<h1>Automobile Inventory</h1>
+					<div className="row gap-5 m-3">
+						<Link to="new" className="btn btn-primary btn-sm col-3">
+							Add a new automobile
+						</Link>
+					</div>
+					<br />
+					<table className="table table-hover text-center">
+						<thead>
+							<tr>
+								<th>Picture</th>
+								<th>Year</th>
+								<th>Manufacturer</th>
+								<th>Model</th>
+								<th>Color</th>
+								<th>VIN</th>
+								<th>Status</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							{automobileList.autos?.map((auto) => {
+								return (
+									<tr className="align-middle" key="" value="">
+										<td>
+											<img className="carpic" src={auto.model.picture_url} />
+										</td>
+										<td>{auto.year}</td>
+										<td>{auto.model.manufacturer.name}</td>
+										<td>{auto.model.name}</td>
+										<td>{auto.color}</td>
+										<td>{auto.vin}</td>
+										<td>{isAvailable(auto.sold)}</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default AutomobilesList;
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -5,6 +5,14 @@ function Nav() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+
+        <NavLink className="navbar-brand" to="/">Manufacturers</NavLink>
+        <NavLink className="navbar-brand" to="/">Add a manufacturer</NavLink>
+        <NavLink className="navbar-brand" to="/">Models</NavLink>
+        <NavLink className="navbar-brand" to="/">Add a vehicle model</NavLink>
+        <NavLink className="navbar-brand" to="/">Automobiles</NavLink>
+        <NavLink className="navbar-brand" to="/">Add an automobile</NavLink>
+
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
